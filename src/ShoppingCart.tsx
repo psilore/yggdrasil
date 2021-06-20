1069c6169264e3c361c2dae091807118f1e1fed5
psilore <psiloc@gmail.com>
2021-06-20 19:21:36 +0200

    added support for local distributed webfonts, nice

--- src/ShoppingCart.tsx
+++ src/ShoppingCart.tsx
@@ -7,54 +7,55 @@ import { css, Styled } from 'react-css-in-js';
 
 export default class ShoppingCart extends React.Component {
 
-    render() {
-        return <Styled>
-          {css`
-            height: 100vh;
-            display: grid;
-            grid-template-columns: 100%;
-            grid-template-rows: auto 1fr auto;
-            grid-template-areas:
-              'header'
-              'main'
-              'footer';
-            @media only screen and (min-width: 992px) {
-              grid-template-columns: 1fr;
-            }
-            img {
-              width: 100%;
-              height: auto;
-            }
-            header {
-              padding: 1em 0;
-            }
-            nav {
-              margin: 0 1em;
-              display: flex;
-              flex-flow: row nowrap;
-            }
-            main {
-              margin: 0 1em;
-            }
-          `}
-          <div>
-            <header>
-              <nav>
-                <Button 
-                  onClick={() => console.log("Button clicked!")}
-                  children= { <Cart name="cart" color="white" size={24} /> }
-                />
-              </nav>
-            </header>
-            <main>
-              <h2>Electonics & Computer peripherals </h2>
-              <p>Lorem ipsum</p>
-              <Products />
-            </main>
-            <footer>
+  render() {
+    return <Styled>
+      {css`
+        height: 100vh;
+        display: grid;
+        grid-template-columns: 100%;
+        grid-template-rows: auto 1fr auto;
+        grid-template-areas:
+          'header'
+          'main'
+          'footer';
+        @media only screen and (min-width: 992px) {
+          grid-template-columns: 1fr;
+        }
+        img {
+          width: 100%;
+          height: auto;
+        }
+        header {
+          padding: 1em 0;
+        }
+        nav {
+          margin: 0 1em;
+          display: flex;
+          flex-flow: row nowrap;
+        }
+        main {
+          margin: 0 1em;
+        }
+      `}
+      <div>
+        <header>
+          <nav>
+            <Button 
+              onClick={() => console.log("Button clicked!")}
+              children= { <Cart name="cart" color="white" size={24} /> }
+            />
+          </nav>
+        </header>
+        <main>
+          <h2>Electronics & Computer peripherals </h2>
+          <p>A diverse collection of the most awesome products on this side of the hemisphere.</p>
+          <h4>Products</h4>
+          <Products />
+        </main>
+        <footer>
 
-            </footer>
-          </div>
-        </Styled>
-    }
+        </footer>
+      </div>
+    </Styled>
+  }
 }
--- src/index.tsx
+++ src/index.tsx
@@ -7,12 +7,48 @@ ReactDOM.render(
  <React.StrictMode>
   <Style>
     {css`
+      @font-face {
+        font-family: 'Poppins';
+        font-style: normal;
+        font-weight: 200;
+        src: local(''),
+            url('./poppins-v15-latin-200.woff2') format('woff2'),
+            url('./poppins-v15-latin-200.woff') format('woff');
+      }
+      @font-face {
+        font-family: 'Poppins';
+        font-style: normal;
+        font-weight: 400;
+        font-display: swap;
+        src: local(''),
+            url('./poppins-v15-latin-regular.woff2') format('woff2'),
+            url('./poppins-v15-latin-regular.woff') format('woff');
+      }
+      @font-face {
+        font-family: 'Poppins';
+        font-style: normal;
+        font-weight: 600;
+        font-display: swap;
+        src: local(''),
+            url('./poppins-v15-latin-600.woff2') format('woff2'),
+            url('./poppins-v15-latin-600.woff') format('woff');
+      }
+      @font-face {
+        font-family: 'Poppins';
+        font-style: normal;
+        font-weight: 800;
+        font-display: swap;
+        src: local(''),
+            url('./poppins-v15-latin-800.woff2') format('woff2'),
+            url('./poppins-v15-latin-800.woff') format('woff');
+      }
       html {
         background-color: rgb(9, 23, 43);
       }
       body {
         margin: 0;
         font-family: Poppins,Helvetica Neue,Helvetica,Segoe UI,Tahoma,Arial,sans-serif;
+        font-weight: 400;
         color: white;
         -webkit-text-size-adjust: 100%;
         -webkit-tap-highlight-color: rgba(0,0,0,0);
